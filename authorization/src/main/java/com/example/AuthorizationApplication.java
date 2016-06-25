4bcc6f53ebf1ea1f87e7aa62d9256bf3b676ab46
making <makingx@gmail.com>
2016-06-26 00:46:16 +0900

    Add /userinfo endpoint

--- authorization/src/main/java/com/example/AuthorizationApplication.java
+++ authorization/src/main/java/com/example/AuthorizationApplication.java
@@ -2,13 +2,25 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableAuthorizationServer
+@EnableResourceServer
+@RestController
 public class AuthorizationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthorizationApplication.class, args);
+	}
+
+	@RequestMapping(path = "/userinfo", method = RequestMethod.GET)
+	Object userinfo(Authentication authentication) {
+		return authentication.getPrincipal();
 	}
 }
