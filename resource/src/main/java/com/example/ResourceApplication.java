3e13190c80f8c6defb26a9d2a41c9fb3ccfc669e
making <makingx@gmail.com>
2016-06-26 01:06:00 +0900

    Secure resource server

--- resource/src/main/java/com/example/ResourceApplication.java
+++ resource/src/main/java/com/example/ResourceApplication.java
@@ -2,8 +2,10 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableResourceServer
 public class ResourceApplication {
 
 	public static void main(String[] args) {
