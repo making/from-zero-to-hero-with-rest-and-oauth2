cdc7d3c9b2bf01a2058425b0558c45fb8532491d
making <makingx@gmail.com>
2016-06-26 02:22:31 +0900

    Add login form

--- authorization/src/main/java/com/example/AuthorizationApplication.java
+++ authorization/src/main/java/com/example/AuthorizationApplication.java
@@ -2,12 +2,18 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EnableAuthorizationServer
@@ -22,5 +28,29 @@ public class AuthorizationApplication {
 	@RequestMapping(path = "/userinfo", method = RequestMethod.GET)
 	Object userinfo(Authentication authentication) {
 		return authentication.getPrincipal();
+	}
+
+	@Configuration
+	static class MvcConfig extends WebMvcConfigurerAdapter {
+		@Override
+		public void addViewControllers(ViewControllerRegistry registry) {
+			registry.addViewController("login").setViewName("login");
+		}
+	}
+
+	@Configuration
+	@Order(-5) // prior to AuthorizationServerSecurityConfiguration (order = 0)
+	static class LoginConfig extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http
+					.requestMatchers()
+					.antMatchers("/login", "/oauth/authorize", "/oauth/confirm_access")
+					.and()
+					.authorizeRequests()
+					.anyRequest().authenticated()
+					.and()
+					.formLogin().loginPage("/login").permitAll();
+		}
 	}
 }
